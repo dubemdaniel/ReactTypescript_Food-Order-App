1566fcb9bd84a972d7070e6c7201689daed04baf
Daniel Chukwudubem <princedaniel7890@gmail.com>
2024-04-29 11:08:11 +0100

    made some correction using flex alignment trick

--- src/components/MealItem.tsx
+++ src/components/MealItem.tsx
@@ -26,14 +26,14 @@ const MealItem: React.FC<Props> = (props) => {
     }
   return (
       <li className='bg-[#1d1a16] overflow-hidden rounded-lg text-center shadow-sm text-white'>
-          <article className='flex flex-col justify-between h-full'>
+          <article className='flex flex-col  h-full gap-2'>
               <img className='object-contain w-[20rem]' src={props.meal.image} alt={ props.meal.name} />
-              <div className='px-[0.2rem]'>
+              <div className='px-[0.2rem] '>
                   <h3 className='font-bold my-[0.75rem]'>{ props.meal.name}</h3>
-                  <p className=' text-[#ffc404] inline-block font-bold rounded-md py-[0.2rem] px-[2rem] bg-[#312c1d]'>{ currencyFormatter.format(props.meal.price)}</p>
-                  <p>{ props.meal.description}</p>
+                  <p className=' text-[#ffc404] inline-block font-bold rounded-md py-[0.2rem] px-[2rem] bg-[#312c1d] mt-auto mb-2' >{ currencyFormatter.format(props.meal.price)}</p>
+                  <p className='my-auto'>{ props.meal.description}</p>
               </div>
-              <p>
+              <p className='mt-auto'>
                   <Button onClick={handleMealToCart}>Add to cart</Button>
               </p>
           </article>
